4d34ea62248927c9c1fd6cc9f3c5e39337d5f544
Ming Duan <11203duan@gmail.com>
2019-03-17 19:57:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
 end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == nil
+    if self.find_by_name(name) == false
       self.create_by_name(name)
     else self.find_by_name(name)
     end
